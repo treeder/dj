b29a3776b7f5c1b51fb9defff11cf4b9d2b4ef5b
CI <treeder+ci@gmail.com>
2017-11-27 20:57:42 +0000

    0.0.6 release [skip ci]

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "Docker Jockey"
 	app.Usage = "Spinning sick containers"
-	app.Version = "0.0.5"
+	app.Version = "0.0.6"
 	app.Action = cli.ShowAppHelp
 
 	app.Commands = []cli.Command{
